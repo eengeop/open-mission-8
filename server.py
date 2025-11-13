170d9fdf88a5859a6464c25f67980281443a10b1
eengeop <leeeus99@gmail.com>
2025-11-13 22:58:22 +0900

    feat: choose refresh inteval

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from crawler import get_stocks
 
 app = Flask(__name__)
 
-@app.route("/")
+default_refresh_interval = 5
+
+@app.route("/", method = "POST")
 def index():
+    global default_refresh_interval
+
+    if request.method == "POST":
+        default_refresh_interval = int(request.form["interval"])
+
     stocks = get_stocks()
-    return render_template("index.html", stocks=stocks)
+
+    return render_template("index.html", stocks=stocks, interval = default_refresh_interval)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
