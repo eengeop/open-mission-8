7260ce74b164759615fb6e60ebbef9844d148da5
eengeop <leeeus99@gmail.com>
2025-11-17 23:19:13 +0900

    feat: get Favorite Stocks

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from crawler import get_stocks
+from database import SessionLocal
+from models import Favorite
 
 app = Flask(__name__)
 
@@ -8,11 +10,17 @@ default_refresh_interval = 5
 @app.route("/", methods = ["GET", "POST"])
 def index():
     global default_refresh_interval
+    db = SessionLocal()
 
     if request.method == "POST":
         default_refresh_interval = int(request.form["interval"])
 
-    return render_template("index.html", interval = default_refresh_interval)
+    favorites = db.query(Favorite).all()
+    favorite_tickers = [f.ticker for f in favorites]
+
+    db.close()
+
+    return render_template("index.html", interval = default_refresh_interval, favorite_tickers = favorite_tickers)
 
 @app.route("/api/stocks")
 def api_stocks():
